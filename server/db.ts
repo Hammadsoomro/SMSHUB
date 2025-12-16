f264c3c9819820d8d6030856a68bb906b3aa08d0
Hammadsoomro <165611609+Hammadsoomro@users.noreply.github.com>
2025-12-17 03:42:30 +0500

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://Hammad:Soomro@cluster0.bqlcjok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  process.env.MONGODB_URI;
 
 let isConnected = false;
 
